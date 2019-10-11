deea219f93c7a90c30f5ff9544d150087b60d4b7
Rethima-Reddy <rethima.wf37@gmail.com>
2019-10-11 12:48:48 -0500

    updated the links

--- java.js
+++ java.js
@@ -16,3 +16,34 @@ const volu = new Vue({
         }
     }
 }) 
+
+
+// for ajax component 
+const jokeURI = 'https://api.icndb.com/jokes/random?limitTo=[nerdy]'
+
+// fetch information
+const getJoke = async () => {
+  try {
+    const response = await fetch(jokeURI)
+    const obj = await response.json()
+    console.log(`FETCHED. Response JSON ${obj}`)
+    const joke = obj.value.joke || 'No joke for you.'
+    return joke
+  } catch (error) { console.error(error) }
+}
+
+// interact with DOM
+const updateWithJoke = async (event) => {
+  try {
+    document.querySelector('#result').innerHTML = ''
+    const answer = await getJoke()
+    document.querySelector('#result').innerHTML = answer
+  } catch (error) { console.error(error) }
+}
+
+
+document.addEventListener('click', event => {
+    if (event.target && event.target.id === 'fetching') { updateWithJoke(event) }
+})
+  
+
